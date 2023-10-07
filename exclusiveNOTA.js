d71554e2afa22cf3f02174adf85d6777e2f9f5e9
xueli0210 <118102239+xueli0210@users.noreply.github.com>
2023-10-07 14:16:17 -0700

    Update exclusiveNOTA.js

--- exclusiveNOTA.js
+++ exclusiveNOTA.js
@@ -8,6 +8,7 @@ const notaCheckbox = document.getElementById('nota');
 // Function to enable or disable the user_detail input based on the other_checkbox state
 function toggleUserDetailInput() {
   userDetailInput.disabled = !otherCheckbox.checked;
+  // when otherCheckbox gets unchecked, userDetailInput gets wiped out
   if (!otherCheckbox.checked) {
     userDetailInput.value = "";
   }
@@ -18,6 +19,9 @@ function toggleUserDetailInput() {
     checkboxes.forEach((checkbox) => {
       checkbox.disabled = notaCheckbox.checked;
     });
+    if (notaCheckbox.checked) {
+    userDetailInput.value = "";
+    }
   }
 
 // Function to check if at least one Q_l12m checkbox is checked
@@ -27,23 +31,24 @@ function isAtLeastOneL12mChecked() {
 
 // Function to handle form submission
 function validateForm(event) {
-  if (!isAtLeastOneL12mChecked()) {
+   if (!isAtLeastOneL12mChecked()) {
     alert("Please select at least one option from 'What did you do in the last 12 months?'");
     event.preventDefault();
   } else if (otherCheckbox.checked && userDetailInput.value.trim() === "") {
     alert("Please provide details in the 'Other (please specify)' field.");
+    event.preventDefault();
+  } else if (!otherCheckbox.checked && !(userDetailInput.value.trim() === "")) {
+    alert("To provide details in the 'Other (please specify)' field you must select it.");
     event.preventDefault();
   }
 }
 
 // Add event listeners
 otherCheckbox.addEventListener('change', toggleUserDetailInput);
-// Add an event listener to the "None of the above" checkbox
 notaCheckbox.addEventListener('change', updateCheckboxes);
 
 // Set initial states
 toggleUserDetailInput();
-// Initialize the checkboxes based on the initial state of the "None of the above" checkbox
 updateCheckboxes();
 
 // Add event listener to the form's submit event
